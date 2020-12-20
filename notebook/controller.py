58bd7931b4d2a1073b619497fded25745ac77146
Robert Haschke <rhaschke@techfak.uni-bielefeld.de>
2020-12-20 11:32:56 +0100

    QP solver

--- notebook/controller.py
+++ notebook/controller.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python
 
 import numpy
+import qpsolvers
 import rospy
 import random
 from std_msgs.msg import Header, ColorRGBA
@@ -113,6 +114,27 @@ class Controller(object):
             N = VN.dot(VN.T)
         self.nullspace = VN  # remember nullspace basis
         return qdot
+
+    def solve_qp(self, equality_tasks, inequality_tasks):
+        """Solve tasks of the form J dq = e  and  J dq ≤ e
+           using quadratic optimization: https://pypi.org/project/qpsolvers"""
+        # combine all equality/inequality tasks into single matrices
+        P = numpy.identity(self.N)  # minimize joint velocity
+        q = numpy.zeros(self.N)
+        A, b = self.stack(equality_tasks) if equality_tasks else (None, None)
+        G, h = self.stack(inequality_tasks) if inequality_tasks else (None, None)
+        self.nullspace = numpy.zeros((self.N, 0))
+        try:
+            result = qpsolvers.solve_qp(P, q, G, h, A, b)
+            if result is None:
+                print("Failed to find a solution")
+        except ValueError as e:
+            print(e)
+            result = None
+        if result is None:
+            return numpy.zeros(self.N)
+        else:
+            return result
 
     @staticmethod
     def stack(tasks):
